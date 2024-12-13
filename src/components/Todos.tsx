8c15a5554f21ca63dddf157bebbe1899c458d6e4
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2024-12-13 18:09:33 +0400

    add some tags into Todos.tsx file

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,8 +1,18 @@
 const Todos: React.FC = () => {
   return (
     <>
-      <input type="text" />
-      <div></div>
+      <input type="text" placeholder="Create a new todo..." />
+      <div>
+        <div>
+          <p>5 items left</p>
+          <p>Clear Completed</p>
+        </div>
+      </div>
+      <div>
+        <p>All</p>
+        <p>Active</p>
+        <p>Completed</p>
+      </div>
     </>
   );
 };
